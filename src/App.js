28ebd7103ae4b703fc98c9eb03c7a1799c71c7c6
Gnuck <ntm32@cornell.edu>
2019-04-17 18:07:50 -0700

    added the current python server address)

--- src/App.js
+++ src/App.js
@@ -3,6 +3,29 @@ import "./App.css";
 import CardList from "./components/CardList.js";
 
 class GotSentiment extends Component {
+  componentWillMount(){
+    this.getData();
+    setInterval(this.getData, 10000);
+  }
+
+  async getData() {
+    try{
+      const res = await fetch('http://18.188.184.54/sampleurl', {
+        method: 'GET',
+        header: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+      });
+      console.log(res);
+      console.log(res.body);
+      res.text().then(data=>console.log(data));
+
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
   render() {
     return (
       <div>
