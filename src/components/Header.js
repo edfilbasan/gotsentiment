517f9cb7dfb2624340f35d9d105c91b903ff4234
Edfil Basan <edgdulay@gmail.com>
2019-04-18 19:00:41 -0700

    navigation

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const container = {
   width: "100%",
-  height: "60px",
+  height: "80px",
   display: "flex",
   flexDirection: "row",
   justifyContent: "space-between",
