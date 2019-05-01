3101990e5ce7d4704bb3ad8236e2b37e95c1e8d2
Edfil Basan <edgdulay@gmail.com>
2019-05-01 12:51:57 -0700

    Updates

---
+++ precache-manifest.1c8e6616097118d9a272adbf0c6eee2d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b11f35f77210c651367969365f87af24",
+    "url": "/gotsentiment/static/media/Trajan Pro Regular.b11f35f7.ttf"
+  },
+  {
+    "revision": "3af178ecd64adfa098e7456e5830e5f8",
+    "url": "/gotsentiment/static/media/Game of Thrones.3af178ec.ttf"
+  },
+  {
+    "revision": "8f234157647e4f1a5ada",
+    "url": "/gotsentiment/static/js/runtime~main.4bd5f926.js"
+  },
+  {
+    "revision": "4baed1d23bbc258161e4",
+    "url": "/gotsentiment/static/js/main.0df20bc7.chunk.js"
+  },
+  {
+    "revision": "cff51fb3894f4fabf414",
+    "url": "/gotsentiment/static/js/2.8a41b8c8.chunk.js"
+  },
+  {
+    "revision": "4baed1d23bbc258161e4",
+    "url": "/gotsentiment/static/css/main.1a70e60f.chunk.css"
+  },
+  {
+    "revision": "cff51fb3894f4fabf414",
+    "url": "/gotsentiment/static/css/2.4bcaad87.chunk.css"
+  },
+  {
+    "revision": "78b62d6a262d8aa5cfe2b7aac9147291",
+    "url": "/gotsentiment/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/gotsentiment/precache-manifest.6d87aca25664754954bf302c7da00b5b.js"
+  "/gotsentiment/precache-manifest.1c8e6616097118d9a272adbf0c6eee2d.js"
 );
 
 workbox.clientsClaim();
