fc41c5e6efbf5b5a00bde2f49131e7db50746c06
Edfil Basan <edgdulay@gmail.com>
2019-05-12 21:03:57 -0700

    Updates

---
+++ precache-manifest.2a3ce614f5c322dab319ec0f975cb168.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b11f35f77210c651367969365f87af24",
+    "url": "/gotsentiment/static/media/Trajan Pro Regular.b11f35f7.ttf"
+  },
+  {
+    "revision": "3af178ecd64adfa098e7456e5830e5f8",
+    "url": "/gotsentiment/static/media/Game of Thrones.3af178ec.ttf"
+  },
+  {
+    "revision": "8f234157647e4f1a5ada",
+    "url": "/gotsentiment/static/js/runtime~main.4bd5f926.js"
+  },
+  {
+    "revision": "c41f911b41640edd3782",
+    "url": "/gotsentiment/static/js/main.4d50c94e.chunk.js"
+  },
+  {
+    "revision": "96bbe900faa5691d5840",
+    "url": "/gotsentiment/static/js/2.6caac579.chunk.js"
+  },
+  {
+    "revision": "c41f911b41640edd3782",
+    "url": "/gotsentiment/static/css/main.1a70e60f.chunk.css"
+  },
+  {
+    "revision": "96bbe900faa5691d5840",
+    "url": "/gotsentiment/static/css/2.4bcaad87.chunk.css"
+  },
+  {
+    "revision": "1d1f052c793a86d0247605981cc5d225",
+    "url": "/gotsentiment/index.html"
+  }
+];
